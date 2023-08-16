6613b3c244576f8622689cbad66582594a85b2f8
Edouard <edouard.legoupil@gmail.com>
2023-08-16 10:51:56 -0500

    fix header name for page...

--- R/header.R
+++ R/header.R
@@ -1,6 +1,7 @@
 header <- function() {
 	dashboardHeader(
-		title = tagList(
-			span(class = 'logo-lg',a("ActivtityInfo Quality",style="color:white !important",href='https://rstudio.unhcr.org/')),
-	) )
+	#	title = tagList(	span(class = 'logo-lg',a("ActivtityInfo Quality",style="color:white !important",href='https://rstudio.unhcr.org/')),	)
+		title =  "ActivtityInfo Quality" 
+	
+	)
 }
